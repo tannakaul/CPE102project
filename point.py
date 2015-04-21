e9aed2f07ae99b676f4b79d1858acf76f175dbc8
Tanna <tkaul@calpoly.edu>
2015-04-21 10:46:36 -0700

    changed create_new_entity back into a regular function to decrease coupling

--- point.py
+++ point.py
@@ -32,26 +32,27 @@ class Point:
    def __init__(self, x, y):
       self.x = x
       self.y = y
-   def create_new_entity(self, entity_select, i_store):
-      name = entity_select + '_' + str(self.x) + '_' + str(self.y)
-      images = image_store.get_images(i_store, entity_select)
-      if entity_select == 'obstacle':
-         return entities.Obstacle(name, self, images)
-      elif entity_select == 'miner':
-         return entities.MinerNotFull(name, MINER_LIMIT,self,
-             random.randint(MINER_RATE_MIN, MINER_RATE_MAX),
-                 images, MINER_ANIMATION_RATE)
-      elif entity_select == 'vein':
-         return entities.Vein(name,
-             random.randint(VEIN_RATE_MIN, VEIN_RATE_MAX), self, images)
-      elif entity_select == 'ore':
-         return entities.Ore(name, self, images,
-             random.randint(ORE_RATE_MIN, ORE_RATE_MAX))
-      elif entity_select == 'blacksmith':
-         return entities.Blacksmith(name, self, images,
-             random.randint(SMITH_LIMIT_MIN, SMITH_LIMIT_MAX),
-                 random.randint(SMITH_RATE_MIN, SMITH_RATE_MAX))
-      else:
-         return None
+
+def create_new_entity(pt, entity_select, i_store):
+   name = entity_select + '_' + str(pt.x) + '_' + str(pt.y)
+   images = image_store.get_images(i_store, entity_select)
+   if entity_select == 'obstacle':
+      return entities.Obstacle(name, pt, images)
+   elif entity_select == 'miner':
+      return entities.MinerNotFull(name, MINER_LIMIT,pt,
+          random.randint(MINER_RATE_MIN, MINER_RATE_MAX),
+              images, MINER_ANIMATION_RATE)
+   elif entity_select == 'vein':
+      return entities.Vein(name,
+          random.randint(VEIN_RATE_MIN, VEIN_RATE_MAX), pt, images)
+   elif entity_select == 'ore':
+      return entities.Ore(name, pt, images,
+          random.randint(ORE_RATE_MIN, ORE_RATE_MAX))
+   elif entity_select == 'blacksmith':
+      return entities.Blacksmith(name, pt, images,
+          random.randint(SMITH_LIMIT_MIN, SMITH_LIMIT_MAX),
+              random.randint(SMITH_RATE_MIN, SMITH_RATE_MAX))
+   else:
+      return None
 
   
